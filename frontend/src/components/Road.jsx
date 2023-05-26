3521901149d426dadcf166344de0012dfe8e4aaa
Jonathan Panzer <jo.panzer11@gmail.com>
2023-05-26 21:30:54 +0200

    Initial Commit, routes, gameMap added

---
+++ frontend/src/components/Road.jsx
@@ -0,0 +1,5 @@
+import "../assets/css/road.css";
+
+export function Road({ color, rotation }) {
+  return <div className="road-box"></div>;
+}
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../assets/css/gameMap.css";
+import { Road } from "../components/Road";
+
+export function Home() {
+  // TODO: place one road on game field
+  return (
+    <div className="page-content">
+      <div className="map-container">
+        <Road />
+      </div>
+    </div>
+  );
+}
