8262ff0fa3c47db8df6e50de1d03cf22c29a36d4
Finnlou13 <108070020+Finnlou13@users.noreply.github.com>
2023-05-27 23:42:23 +0200

    hab halt backend gemahct

---
+++ backend/index.js
@@ -0,0 +1,5 @@
+class Game{
+    constructor(amountTeams){
+        this.colors = ["red","blue","green","yellow","orange","black","white"]
+    }
+}
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import { Home } from "./pages/Home";
+import PointShop from "./pages/PointShop";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/map" element={<Home />} />
+      <Route path="/shop" element={<PointShop />} />
     </Routes>
   );
 }
