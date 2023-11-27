1b5f9fa08558ebeb25c950c2cb76e9ea355b9e64
Finnlou13 <108070020+Finnlou13@users.noreply.github.com>
2023-11-27 19:11:02 +0100

    relocated roads.json and added a function to extract data from it.

--- frontend/src/components/Road.jsx
+++ frontend/src/components/Road.jsx
@@ -1,5 +1,13 @@
 import "../assets/css/road.css";
-
+import jsondata from "./roads.json"
 export function Road({ color, rotation }) {
+    let road = Collect_Road_data("Denver - Kansas");
   return <div className="road-box"></div>;
+}
+
+function Collect_Road_data(roadName) {
+    let roads = jsondata;
+    let output = roads[roadName];
+    console.log(output);
+    return output;
 }
