5cc12b03d67b99827b4ebb36eb23adad702308d1
Jonathan Panzer <jo.panzer11@gmail.com>
2023-12-23 16:56:00 +0100

    added get roote for uuIDs in backend

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,17 @@
 // node imports
 const http = require('http');
 const express = require('express');
-var cors = require('cors')
+const cors = require('cors')
+const fs = require("fs");
 
 // javascript imports
-const Game = require('./game-components/game.js');
+const Game = require('./gameComponents/game.js');
+const getUUID = require("./utilityFunctions/getUUID")
 
 // setting variables
 const port = 3000;
 const game = new Game(6)
+const usedUUIDs = JSON.parse(fs.readFileSync("./UUID.json"));
 
 // building app
 const app = express();
@@ -18,6 +21,7 @@ app.use(express.json())
 app.use(cors())
 
 // routers
+
 app.get("/game/state", (req, res) => {
     try {
         let json = game.jsonResponse()
@@ -45,6 +49,16 @@ app.get("/game/end", (req, res) => {
     }
 })
 
+app.get("/game/unique-random-user-ID", (req, res) => {
+    try {
+        let uuid = getUUID(usedUUIDs.UUIDs);
+        fs.writeFileSync("./UUID.json", JSON.stringify(usedUUIDs))
+        res.status(200).json({ "uuid": uuid })
+    } catch (err) {
+        res.status(500);
+    }
+})
+
 app.get("/teams", (req, res) => {
     try {
         res.status(200).json(game.jsonTeamResponse())
@@ -54,5 +68,9 @@ app.get("/teams", (req, res) => {
 })
 
 
+
+
 // app listens on port
 app.listen(port, () => console.log(`server started on http://localhost:${port}`));
+
+module.exports = game
