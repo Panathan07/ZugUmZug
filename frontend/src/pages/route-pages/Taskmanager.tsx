8576fcd22340f47d27dad836eaef649ed038ed96
Panathan07 <108070166+Panathan07@users.noreply.github.com>
2024-03-01 17:37:01 +0100

    Revert "Tasks"

--- frontend/src/pages/route-pages/Taskmanager.tsx
+++ frontend/src/pages/route-pages/Taskmanager.tsx
@@ -1,58 +1,20 @@
-import "../../assets/css/taskmanager.css";
-import { useTasksData } from "../../hooks/useTasksData"
-import { LoadingPage } from "../state-pages/LoadingPage";
-import { PendingTaskCard } from "../../components/tasks/taskCard"
-import { AcceptedTaskCard } from "../../components/tasks/taskCard"
-import { cardTask } from "../../custom-types/gameTask"
-
+import "@assets/css/taskmanager.css";
 export function Taskmanager() {
-    
-    const data = useTasksData()
-    if (data == null) {
-        return <LoadingPage/>
-    }
-
-    const pending_task_arr: cardTask[] = []
-    for (const value of Object.values(data.pending)) {
-        pending_task_arr.push(value)
-    }
-    const accepted_task_arr: cardTask[] = []
-    for (const value of Object.values(data.accepted)) {
-        accepted_task_arr.push(value)
-    }
-    return (
-        <>
-            <div className = "title-box" >
-                <p className="title">Aufgaben</p>
-            </div>
-            <div className="task-box">
-                <p style={{ fontSize: "17px", display: "inline" }}>Angenommene Aufgaben</p>
-                <div className="task-grid">
-                    {accepted_task_arr.map((task_value) => (
-                        <section>
-                            <AcceptedTaskCard
-                                name={task_value.name}
-                                description={task_value.description}
-                                data={task_value.data}
-                            />
-                        </section>
-                    ))}
-
-                </div>
-            </div>
-            <div className="task-box">
-                <p style={{ fontSize: "17px", display: "inline" }}>Anzunehmende Aufgaben</p>
-                <div className="task-grid">
-                    {pending_task_arr.map((task_value) => (
-                        <PendingTaskCard
-                            name={task_value.name}
-                            description={task_value.description}
-                            data={task_value.data }
-                            />
-                    ))}
-
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="title-box">
+        <p className="title">Aufgaben</p>
+      </div>
+      <div className="task-box">
+        <p style={{ fontSize: "17px", display: "inline" }}>
+          Angenommene Aufgaben
+        </p>
+      </div>
+      <div className="task-box">
+        <p style={{ fontSize: "17px", display: "inline" }}>
+          Anzunehmende Aufgaben
+        </p>
+      </div>
+    </>
+  );
 }
